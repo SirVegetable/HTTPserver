1b760f08a59b42f0d1f582661c37047a39d4400c
SirVegetable <17wgs@queensu.ca>
2024-05-14 16:45:38 -0400

    implementation file for the server

---
+++ server.cpp
@@ -0,0 +1,31 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string> 
+#include "server.h"
+
+Server::Server()
+{
+
+}
+
+Server::~Server()
+{
+
+}
+
+void Server::listen()
+{
+
+}
+std::string Server::receive_message()
+{
+
+}
+void Server::send_response()
+{
+
+}
+void Server::disconnect()
+{
+    
+}
